8c565c5212d22d58c21e20ae5ac356b3d23c715c
apollox-docs <93172934+apollox-docs@users.noreply.github.com>
2023-03-15 01:13:34 +0300

    Update ApxRewardFacet.sol

--- contracts/diamond/facets/ApxRewardFacet.sol
+++ contracts/diamond/facets/ApxRewardFacet.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import "../../utils/Constants.sol";
 import "../interfaces/IApxReward.sol";
 import "../libraries/LibApxReward.sol";
 import "../libraries/LibAccessControlEnumerable.sol";
@@ -8,19 +9,17 @@ import "../libraries/LibAccessControlEnumerable.sol";
 // calculate apx award
 contract ApxRewardFacet is IApxReward {
 
-    bytes32 public constant STAKE_OPERATOR_ROLE = keccak256("STAKE_OPERATOR");
-
     function initializeApxRewardFacet(address _rewardsToken, uint256 _apxPerBlock, uint256 _startBlock) external {
         require(_rewardsToken != address(0), "Invalid _rewardsToken");
         require(_apxPerBlock >= 0, "Invalid _apxPerBlock");
         require(_startBlock >= 0, "Invalid _startBlock");
 
-        LibAccessControlEnumerable.checkRole(LibAccessControlEnumerable.DEPLOYER_ROLE);
+        LibAccessControlEnumerable.checkRole(Constants.DEPLOYER_ROLE);
         LibApxReward.initialize(_rewardsToken, _apxPerBlock, _startBlock);
     }
 
     function updateApxPerBlock(uint256 _apxPerBlock) external override {
-        LibAccessControlEnumerable.checkRole(STAKE_OPERATOR_ROLE);
+        LibAccessControlEnumerable.checkRole(Constants.STAKE_OPERATOR_ROLE);
         LibApxReward.updateApxPerBlock(_apxPerBlock);
     }
 
