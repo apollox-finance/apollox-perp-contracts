4c7b2e7ceff15556d750e5b088e94756a50c397e
apollox-docs <93172934+apollox-docs@users.noreply.github.com>
2023-03-15 01:15:33 +0300

    Update DiamondCutFacet.sol

--- contracts/diamond/facets/DiamondCutFacet.sol
+++ contracts/diamond/facets/DiamondCutFacet.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import "../../utils/Constants.sol";
 import "../interfaces/IDiamondCut.sol";
 import "../libraries/LibDiamond.sol";
 import "../libraries/LibAccessControlEnumerable.sol";
@@ -8,7 +9,7 @@ import "../libraries/LibAccessControlEnumerable.sol";
 contract DiamondCutFacet is IDiamondCut {
 
     function diamondCut(FacetCut[] calldata _diamondCut, address _init, bytes calldata _calldata) external override {
-        LibAccessControlEnumerable.checkRole(LibAccessControlEnumerable.DEPLOYER_ROLE);
+        LibAccessControlEnumerable.checkRole(Constants.DEPLOYER_ROLE);
         LibDiamond.diamondCut(_diamondCut, _init, _calldata);
     }
 }
