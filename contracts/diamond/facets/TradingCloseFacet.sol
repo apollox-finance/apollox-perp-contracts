f2207068c9168b279054dcaf339a07b6f664b47b
apollox-docs <93172934+apollox-docs@users.noreply.github.com>
2023-04-17 14:07:18 +0300

    Update TradingCloseFacet.sol

--- contracts/diamond/facets/TradingCloseFacet.sol
+++ contracts/diamond/facets/TradingCloseFacet.sol
@@ -35,7 +35,7 @@ contract TradingCloseFacet is ITradingClose, OnlySelf {
         LibTrading.TradingStorage storage ts, OpenTrade storage ot,
         bytes32 tradeHash, uint256 marketPrice, uint256 closePrice
     ) private returns (IOrderAndTradeHistory.CloseInfo memory) {
-        int256 longAccFundingFeePerShare = ITradingCore(address(this)).updatePairPositionInfo(ot.pairBase, closePrice, marketPrice, ot.qty, ot.isLong, false);
+        int256 longAccFundingFeePerShare = ITradingCore(address(this)).updatePairPositionInfo(ot.pairBase, ot.entryPrice, marketPrice, ot.qty, ot.isLong, false);
         IVault.MarginToken memory mt = IVault(address(this)).getTokenForTrading(ot.tokenIn);
         int256 fundingFee = LibTrading.calcFundingFee(ot, mt, marketPrice, longAccFundingFeePerShare);
         uint256 closeNotionalUsd = closePrice * ot.qty;
@@ -312,7 +312,7 @@ contract TradingCloseFacet is ITradingClose, OnlySelf {
                     emit ExecuteCloseRejected(ot.user, t.tradeHash, ExecutionType.LIQ, t.price, marketPrice);
                     continue;
                 }
-                ITradingCore(address(this)).updatePairPositionInfo(ot.pairBase, closePrice, marketPrice, ot.qty, ot.isLong, false);
+                ITradingCore(address(this)).updatePairPositionInfo(ot.pairBase, ot.entryPrice, marketPrice, ot.qty, ot.isLong, false);
                 _executeLiquidate(ts, ot, t.tradeHash, closePrice, pnl, fundingFee, closeFee);
             }
         }
